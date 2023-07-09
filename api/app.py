74b60f2464dcc13da5f170d98af12f88547bd45b
voynow <voynow99@gmail.com>
2023-07-09 09:53:25 -0400

    Testing out app.py logging

--- api/app.py
+++ api/app.py
@@ -11,12 +11,19 @@ socketio = SocketIO(app, cors_allowed_origins="*")
 
 @app.route('/portfolio')
 def portfolio():
-    return services.fetch_portfolio()
+    resp = services.fetch_portfolio()
+    # console log resp
+    app.logger.info(resp)
+    return resp
 
 @app.route('/portfolio/<string:project_name>')
 def get_project_details(project_name):
     portfolio = {project['name']: project for project in services.fetch_portfolio()}
-    return portfolio.get(project_name)
+    resp = portfolio.get(project_name)
+    app.logger.info(project_name)
+    app.logger.info(resp)
+    return resp
+
 
 @socketio.on('send_message')
 def handle_message(data):
